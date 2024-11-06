d4461c44f50563df81064e2563d20718434d9804
manohySr <manohyrajaonah@gmail.com>
2024-11-06 21:23:25 +0400

    update

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -8,7 +8,31 @@ export class UsersService {
   constructor(@InjectRepository(User) private repo: Repository<User>) {}
 
   create(email: string, password: string) {
-    const user = this.repo.create({ email, password });
+    const newUser = this.repo.create({ email, password });
+    return this.repo.save(newUser);
+  }
+
+  async findOne(id: number) {
+    const user = await this.repo.findOne({ where: { id } });
+    if (!user) {
+      throw new Error("user can't be found");
+    }
+    return user;
+  }
+
+  find(email: string) {
+    return this.repo.find({ where: { email } });
+  }
+
+  async update(id: number, attrs: Partial<User>) {
+    const user = await this.findOne(id);
+
+    Object.assign(user, attrs);
     return this.repo.save(user);
+  }
+
+  async remove(id: number) {
+    const user = await this.findOne(id);
+    return this.repo.remove(user);
   }
 }
